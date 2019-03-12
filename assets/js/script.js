1477cd234fce2d70c32bc78a79c28470108905c7
HCdev134 <cdev12ucds@gmail.com>
2019-03-11 22:57:18 -0500

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// var counter = 0; 
+
+// // re visit https://  for info on counter 
+// function handleCounter(){
+//    var countNum =  document.getElementById("count-number");
+//    counter++; 
+
+// }
+
+// function handleGreeting() {
+//     var welcome = "Hello and Welcome to My Website..."
+//     document.getElementById("greeter").innerText = `( : ${welcome}`; 
+// }
+
+var imgFx = $("<img></img>"); 
+console.log(imgFx); 
+
+// select child elements using chaining and attributes 
+
+var parentOfImg = $("#images"); 
+
